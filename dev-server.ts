1045d50a940963d1ae665c78d5b4585a0cf7753d
maxdevos49 <maxdevos49@gmail.com>
2020-06-11 15:14:46 -0500

    abandoning work on setting up testing because almost all testing frameworks do not seem support esm and typescript together

--- dev-server.ts
+++ dev-server.ts
@@ -1,11 +1,12 @@
 import http from "http";
 import express from "express";
 import ip from "ip";
+import path from "path";
 
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 
-app.use(express.static(__dirname + "/src"));
+app.use(express.static(path.resolve() + "/src"));
 
 app.use((_, res) => {
     res.redirect("/index.html");
